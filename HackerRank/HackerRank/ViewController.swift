6d0a727cbead201bc859163a12f7cb6c0258404c
MiguelOtero <miguelangel.oterocalderon@externos-cl.cencosud.com>
2022-08-14 09:23:13 -0500

    solutions in proccesing

--- HackerRank/HackerRank/ViewController.swift
+++ HackerRank/HackerRank/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        FunctionMin.test()
+        CountingSort.test()
         self.view.backgroundColor = .red
     }
 }
