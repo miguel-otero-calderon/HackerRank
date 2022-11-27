774a891cb9b7ef61f7ec479b664347480976b492
Miguel Otero <miguelotero@MacBook-Pro-Miguel-150121.Local>
2022-11-27 18:02:35 -0500

    Avance

--- HackerRank/HackerRank/UseCase/MergeTwoSortedLists.swift
+++ HackerRank/HackerRank/UseCase/MergeTwoSortedLists.swift
@@ -13,7 +13,7 @@ struct MergeTwoSortedLists {
         var list1 = MergeTwoSortedLists.ListNode(1)
         list1.next = MergeTwoSortedLists.ListNode(2)
         list1.next?.next = MergeTwoSortedLists.ListNode(4)
-
+        
         var list2 = MergeTwoSortedLists.ListNode(1)
         list2.next = MergeTwoSortedLists.ListNode(3)
         list2.next?.next = MergeTwoSortedLists.ListNode(4)
@@ -22,14 +22,45 @@ struct MergeTwoSortedLists {
         
     }
     static func mergeTwoLists(_ list1: MergeTwoSortedLists.ListNode?, _ list2: MergeTwoSortedLists.ListNode?) -> MergeTwoSortedLists.ListNode? {
+        if list1 == nil && list2 != nil {
+            return list2
+        }
+        if list1 != nil && list2 == nil {
+            return list1
+        }
+        guard let list1 = list1 , let list2 = list2 else {
+            return nil
+        }
+        var current1: MergeTwoSortedLists.ListNode?
+        var current2: MergeTwoSortedLists.ListNode?
+        
+        current1 = list1
+        current2 = list2
+        
+        while current1 != nil {
+            
+        }
+        
         return nil
     }
     
     public class ListNode {
-          public var val: Int
-          public var next: ListNode?
-          public init() { self.val = 0; self.next = nil; }
-          public init(_ val: Int) { self.val = val; self.next = nil; }
-          public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
-     }
+        public var val: Int
+        public var next: ListNode?
+        
+        public init() {
+            self.val = 0
+            self.next = nil
+        }
+        
+        public init(_ val: Int) {
+            self.val = val
+            self.next = nil
+        }
+        
+        public init(_ val: Int, _ next: ListNode?) {
+            self.val = val
+            self.next = next
+        }
+    }
 }
